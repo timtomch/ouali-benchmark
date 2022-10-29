d89893a06d970e2daa9e97129f1277c67e888f94
Thomas Guignard <tom@timtom.ch>
2022-10-28 22:55:00 -0400

    Script comptage minimal
    Affichage des résultats pas idéal mais ça fonctionne

--- ouali-stats.py
+++ ouali-stats.py
@@ -27,15 +27,18 @@ counts = {}
 for infile in ouali_established_files:
     instance_name = re.search(r'(\w+-\w+)',infile).group()
     ouali_data = pd.read_csv(infile, sep='\t', dtype = str)
-    ouali_counts = ouali_data['décision d\'alignement'].value_counts(sort=False)
-    counts.update({instance_name:ouali_counts})
+    ouali_align = ouali_data.query('`nombre de candidats` != "0"').copy()
+    ouali_counts = ouali_align['décision d\'alignement'].value_counts(sort=False)
+    ouali_notalign = ouali_data.query('`nombre de candidats` == "0"').copy()
+    ouali_notcounts = ouali_notalign['décision d\'alignement'].value_counts(sort=False)
+    counts.update({instance_name:{"Alignements":ouali_counts,"Non-concordances":ouali_notcounts}})
 
 count_table = pd.DataFrame(counts)
 
 
 for infile in ouali_undefined_files:
     instance_name = re.search(r'(\w+-\w+)',infile).group()
-    ouali_data = pd.read_csv(infile, sep='\t', dtype = str).size
+    ouali_data = pd.read_csv(infile, sep='\t', dtype = str).shape[0]
     count_table.at["à faire",instance_name] = ouali_data
 
 print(tabulate(count_table.T,headers="keys"))
